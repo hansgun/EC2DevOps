397feb4539d7e05d431c4eb64dc5f742a51fa49d
hansgun <gmos@nate.com>
2020-06-23 14:29:21 +0900

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,7 @@
 # crontab -l | { cat; echo "@reboot pm2 start /var/www/index.js -i 0 --name \"node-app\""; } | crontab -
 # sudo pm2 start /var/www/index.js -i 0 --name "node-app"
 
-cd /home/ubuntu/FirstNodeApp
-sudo npm install
+#cd /home/ubuntu/FirstNodeApp
+#sudo npm install
 pm2 stop index.js
 pm2 start index.js
